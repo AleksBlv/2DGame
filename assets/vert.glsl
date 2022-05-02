0796035a850f92c41ffc72bfbe12aeecc09ff24f
Aleksey Belyaev <aleks_blv@mail.ru>
2022-05-02 21:21:17 +0300

    vertex shader

--- assets/vert.glsl
+++ assets/vert.glsl
@@ -1,15 +1,17 @@
 #version 410
 
 layout(location = 0) in vec3 vertexPosition;
-layout(location = 1) in vec3 vertexColor;
-layout(location = 2) in vec2 textureCoord;
+layout(location = 2) in vec3 vertexColor;
+layout(location = 1) in vec2 textureCoord;
 
 out vec3 color;
 out vec2 texCoord;
-uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main(){
    color = vertexColor;
-   gl_Position =transform * vec4(vertexPosition, 1.0);
+   gl_Position =projection * view * model * vec4(vertexPosition, 1.0);
    texCoord = textureCoord;
 }
